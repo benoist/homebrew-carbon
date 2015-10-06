09fa7ca3ff3c57223e96d0b706254fe5b7219607
Benoist Claassen <benoist.claassen@gmail.com>
2015-10-06 20:03:43 +0200

    use release

--- carbon.rb
+++ carbon.rb
@@ -1,11 +1,15 @@
 require "formula"
 
-class Carbon < Formula
+class Shards < Formula
   homepage "https://github.com/benoist/carbon-crystal"
   head "https://github.com/benoist/carbon-crystal.git"
+  url "https://github.com/benoist/carbon-crystal/archive/v0.1.0.tar.gz"
+  sha256 "2a4395881499cb260791fae42f95dd45ec351bbca4f1d99f3f1dc76cd54ceb16"
+
+  depends_on "crystal-lang" => :build
 
   def install
-    system "crystal", "build", "-o", "bin/carbon", "src/command.cr"
+    system "crystal", "build", "-o", "bin/carbon", "src/command.cr", "--release"
     prefix.install "bin"
   end
 end
